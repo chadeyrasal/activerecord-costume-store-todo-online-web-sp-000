a6482d861f30cba93c20abd65c85d128ae3fd028
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-10 17:04:41 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,11 @@
 # Create your haunted_houses migration here
 
 class HauntedHouse < ActiveRecord::Migration[5.1]
+
   def change
+    create_table :haunted_houses do |t|
+      
+    end
   end
+
 end
