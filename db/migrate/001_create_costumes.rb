24f61c8fc30f7eb33ea9dab9fd719a7e38688b4f
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-10 16:55:54 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,11 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.float :
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 
