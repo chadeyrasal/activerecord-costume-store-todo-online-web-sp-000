ed669acad7c8613493e0cc919eb020d747bc78e8
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-10 17:02:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.sting :name
       t.string :location
-      
+      t.integer :costume_inventory
+      t.
     end
   end
 
