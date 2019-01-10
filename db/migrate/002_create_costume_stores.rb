cf20b05fd1e6f763d7ad5e195b741f346e34548b
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-10 17:13:53 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,8 +9,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.timestamp :opening_time
-      t.timestamp :closing_time
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 
